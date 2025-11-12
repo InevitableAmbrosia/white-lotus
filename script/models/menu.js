34183e57f423d8fa9196b9d55b9625e31a3c7b4b
InevitableAmbrosia <christianjosiahperrone@gmail.com>
2025-11-12 18:19:10 -0500

    Add files via upload

---
+++ script/models/menu.js
@@ -0,0 +1,9 @@
+const menu = {
+	"thinkers" : ["Heraclitus", "Socrates", "Plato", "Descartes", "Hume", "Kant", "Hegel",  "Nietzsche","Bertrand Russell"]
+	,
+	"sutras" : ["Nirvana Sutra", "Exalted Sublime Golden Light", "Heart Sutra", "Avatamsaka Sutra", "The Lotus Sutra", "Diamond Sutra", "Lankavatara Sutra", "The Dhammapada"]
+	,
+	"buddhist_glossary" : ["anatta", "Maya", "Yogacara", "Dharma", "Emptiness", "Skillful Means", "Transience", "Omniscience"],
+	"hermetic_glossary" : ["Sun", "Matter", "Form", "Eternity", "Wisdom", "Morality", "Logic",
+		"Self"]
+}
---
+++ script/models/white_lotus.js
@@ -0,0 +1,10 @@
+const white_lotus = {
+	"home_description" : "The Lotus Sutra speaks of a Single Vehicle, leading all true knowers to Omniscience. "
+	+ "The previous teachings, like the world, are only temporary! "
+	 + "What if all of Western Philosophy is just a tentative teaching, "
+	 + "and Enlightenment is the same in all places?"
+
+	,
+	"navigation_instructions" : "Use the sidebar to navigate the Glossary of terms, to connect all of Western philosophy to the End of Knowledge!",
+	"engineering" : "This site uses an in-house Router alongside a JSON graph database to connect various quotations from Philosophy and Hermetics to Mahayana Buddhism."
+}
